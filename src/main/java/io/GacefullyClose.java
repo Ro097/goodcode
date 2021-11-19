6e204756e65a81ad801d6a723a8852afc14b57cd
luojun <1983098920@qq.com>
2021-11-19 23:10:12 +0800

    java io close 2

--- src/main/java/io/GacefullyClose.java
+++ src/main/java/io/GacefullyClose.java
@@ -7,12 +7,17 @@ import java.io.IOException;
 /**
  * 该文档介绍如何优雅关闭IO流。
  *
- *
  * @author luojun
  * @create 2021-11-19 21:25
  */
 public class GacefullyClose {
     public static void main(String[] args) {
+        // TODO: 2021/11/19
+        /*
+        try-with-resource 语句能确保每个资源在语句结束时关闭。所用实现了
+        java.lang.AutoCloseable接口（其中它包括实习了java.io.Closeable的所有对象）
+         */
+
         try (FileInputStream in = new FileInputStream(new File("D:\\bigdata\\0.luojun\\2.workspaces\\goodcode\\input\\io_test.txt"))){
             byte[] b = new byte[1025];
             int line = 0;
